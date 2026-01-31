5913b38984e25b76542ffeb54f42f5b366bad6b3
Gustavo B550M <g236249@dac.unicamp.br>
2026-01-31 07:30:37 -0300

    Add final comments

--- camera.py
+++ camera.py
@@ -1,15 +1,36 @@
 import cv2 as cv
+import numpy as np
+from typing import Tuple
 
 class Camera:
+    """
+    Camera control using OpenCV.
+    """
 
     def __init__(self, camera_id:int=0):
+        """
+        Initialize the camera.
+        
+        :param camera_id: Index of the camera device
+        :type camera_id: int
+        """
+
         self.cap = cv.VideoCapture(camera_id)
         if not self.cap.isOpened():
             raise RuntimeError("Could not open camera")
         
-    def read(self):
+    def read(self) -> Tuple[bool, np.ndarray]:
+        """
+        Read a frame from the camera.
+        
+        :return: Current frame
+        :rtype: Tuple[bool, np.ndarray]
+        """
         return self.cap.read()
     
     def release(self):
+        """
+        Release the camera resource.
+        """
         self.cap.release()
     
