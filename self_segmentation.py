c3c49241962e5af33cde1cfd6d48876ecea21451
Gustavo B550M <g236249@dac.unicamp.br>
2026-02-08 13:33:59 -0300

    Not finish

--- self_segmentation.py
+++ self_segmentation.py
@@ -2,13 +2,22 @@ import cv2 as cv
 import numpy as np
 import mediapipe as mp
 
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+
+from mediapipe import Image, ImageFormat
+
 class SelfSegmentationTools:
 
-    def __init__(self):
+    def __init__(self, model:"selfie_segmenter.tflite"):
         
-        self.mp_selfie = mp.solutions.self_segmentation
-        self.segmenter = self.mp_selfie.SelfSegmentation(model_selection=1)
+        self.base_options = python.BaseOptions(model_asset_path=model)
+        self.options = vision.ImageSegmenterOptions(base_options=self.base_options,
+                                       output_category_mask=True)
+        self.segmenter = vision.ImageSegmenter.create_from_options(self.options)
 
+    # TODO - not finished
+    # Access this links - https://ai.google.dev/edge/mediapipe/solutions/vision/image_segmenter/python#live-stream
     def segment_foreground(self, frame:np.ndarray) -> np.ndarray:
         rgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
         result = self.segmenter.process(rgb)
