2f482ea51edfd1b50cf03a76454d53f640d02ec6
Gustavo B550M <g236249@dac.unicamp.br>
2026-02-08 12:08:45 -0300

    Add skin mask and depth filter

--- camera.py
+++ camera.py
@@ -28,6 +28,39 @@ class Camera:
         """
         return self.cap.read()
     
+    # TODO - not good - too strong
+    def depth_like_filter(self, frame) -> np.ndarray:
+
+        frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+        frame_blur = cv.GaussianBlur(frame_gray, (9, 9), 0)
+
+        frame_edges = cv.Canny(frame_blur, 50, 120)
+
+        kernel = np.ones((5, 5), np.uint8)
+        edges = cv.dilate(frame_edges, kernel, iterations=2)
+
+        mask = edges > 0
+
+        result = frame.copy()
+        result[~mask] = (0, 0, 0) # destroy background
+
+        return result
+    
+    def skin_mask(self, frame) -> np.ndarray:
+        ycrcb = cv.cvtColor(frame, cv.COLOR_BGR2YCrCb)
+
+        lower = np.array([0, 135, 85])
+        upper = np.array([255, 180, 135])
+
+        mask = cv.inRange(ycrcb, lower, upper)
+
+        kernel = np.ones((5, 5), np.uint8)
+        mask = cv.morphologyEx(mask, cv.MORPH_OPEN, kernel)
+        mask = cv.morphologyEx(mask, cv.MORPH_DILATE, kernel)
+
+        return mask
+
     def release(self):
         """
         Release the camera resource.
