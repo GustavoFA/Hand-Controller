1514ad3013eaa1b8c991b0df3d0716d7ac751897
Gustavo B550M <g236249@dac.unicamp.br>
2026-02-09 09:41:20 -0300

    Fix controller response time

--- app.py
+++ app.py
@@ -10,18 +10,18 @@ class HandControlApp:
 
     def __init__(self):
         self.camera = Camera()
-        self.detector = HandTracker(num_hands=2)
+        self.detector = HandTracker(mode='video', num_hands=1)
         self.controller = ComputerInputController()
-        # self.segmenter_tool = SelfSegmentationTools()
+        # self.segmenter_tool = SelfSegmentationTools() # to be explored
 
-    def run_controller_for_game(self, minimum_hand_score:float=0.5, debounce:float=0.3):
+    def run_controller_for_game(self, minimum_hand_score:float=0.5, debounce:float=0.0001):
         commands = {
             'index': 'space',
             'thumb': 'd',
             'pinky': 'a',
             'middle': 'w'
         }
-
+        key_states = {finger: False for finger in commands}
         input('Press ENTER to start the controller:\n')
         last_time = time.time()
         while True:
@@ -29,17 +29,21 @@ class HandControlApp:
             if not ret:
                 print('Failed to read frame')
                 break
-            
-            self.detector._detect_async(frame)
-            if self.detector.update_knuckles_coordinates(minimum_hand_score, verbose=False) and (time.time() - last_time > debounce):
-                keys_status = {}
-                for finger, button in commands.items():
-                    if self.detector.is_finger_extended(finger):
-                        keys_status[button] = True
-                    else:
-                        keys_status[button] = False
-                self.controller.controller_buttons(keys_status)
-                last_time = time.time()
+            self.detector.get_results(frame)
+
+            if not self.detector.update_knuckles_coordinates(minimum_hand_score, verbose=False):
+                continue
+
+            for finger, key in commands.items():
+                extended = self.detector.is_finger_extended(finger)
+                # PRESS once
+                if extended and not key_states[finger]:
+                    pyautogui.keyDown(key)
+                    key_states[finger] = True
+                # RELEASE once
+                elif not extended and key_states[finger]:
+                    pyautogui.keyUp(key)
+                    key_states[finger] = False
         self.cleanup()
 
     def run_test(self):
@@ -56,7 +60,7 @@ class HandControlApp:
             # frame = self.segmenter_tool.segment_foreground(frame)
 
             # update the hand position
-            self.detector.detect_async(frame)
+            # self.detector.detect_async(frame) # not support anymore
 
             # results = self.detector.get_results()
 
@@ -70,7 +74,7 @@ class HandControlApp:
             if self.detector.update_knuckles_coordinates(0.5) and (time.time() - run_time > 1.0):
                 run_time = time.time()
 
-                print(self.detector.HAND_KNUCKLES_COORDINATES[0])
+                # print(self.detector.HAND_KNUCKLES_COORDINATES[0])
 
                 # INDEX_F = self.detector.HAND_KNUCKLES_COORDINATES[5:9]
                 # # print("\nCOORDINATES:")
@@ -83,9 +87,9 @@ class HandControlApp:
 
                 # To identify 
                 print(10*'-')
-                # for key in self.detector.FINGER_INDEX.keys():
-                #     if self.detector.is_finger_extended(key):
-                #         print(key)
+                for key in self.detector.FINGER_INDEX.keys():
+                    if self.detector.is_finger_extended(key):
+                        print(key)
             
             # print(f'\n\n{results}\n\n{len(results)}\n\n')
 
